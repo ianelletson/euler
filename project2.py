135107a1c58eacb4b2555f7cc2f4951a451c06c5
ianelletson <ianelletson@gmail.com>
2013-06-18 11:12:21 -0500

    Changed variable declaration and indentation

--- project2.py
+++ project2.py
@@ -6,10 +6,11 @@ def fib(exceed):
     fibCount2 = 1
     fibCount3 = 0
     fibSum = 0
+    i = 0
     while i < exceed:
         fibCount3 = fibCount1 + fibCount2
         fibCount1 = fibCount2
         fibCount2 = fibCount3
         if (fibCount3 % 2 == 0):
             fibSum = fibSum + fibCount3
-        print(fibSum)
+    print(fibSum)
