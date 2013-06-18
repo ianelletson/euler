806ab49e6058660cde33a4a6b84fb0dc440c86f7
ianelletson <ianelletson@gmail.com>
2013-06-18 11:10:06 -0500

    First attempt at problem 2

--- project2.py
+++ project2.py
@@ -1,1 +1,15 @@
 #Project 2
+
+#fib: int -> void
+def fib(exceed):
+    fibCount1 = 0
+    fibCount2 = 1
+    fibCount3 = 0
+    fibSum = 0
+    while i < exceed:
+        fibCount3 = fibCount1 + fibCount2
+        fibCount1 = fibCount2
+        fibCount2 = fibCount3
+        if (fibCount3 % 2 == 0):
+            fibSum = fibSum + fibCount3
+        print(fibSum)
