8e4a6a118429de6023912a74470a660c27825964
ianelletson <ianelletson@gmail.com>
2013-06-18 13:28:04 -0500

    Testing sum implementation

--- project2.py
+++ project2.py
@@ -5,10 +5,13 @@ def fib(limit):
     num1 = 0
     num2 = 1
     num3 = 0
+    fibSum = 0
     while True:
         num3 = num1 + num2
         num1 = num2
         num2 = num3
-        print(num2)
         if (num3 >= limit):
             break
+        if (num3 % 2 == 0):
+            fibSum = fibSum + num3
+    print(fibSum)
