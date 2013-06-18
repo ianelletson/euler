4591b083d6b43711b74ac13f82ab044199b62f64
ianelletson <ianelletson@gmail.com>
2013-06-18 09:14:31 -0500

    reordered code, was getting function not defined

--- project1.py
+++ project1.py
@@ -2,16 +2,16 @@
 
 sum = 0
 
-for i in range(0, 1000):
-    if (div_by(i, 3)):
-        sum = sum + i
-    elif (div_by(i, 5)):
-        sum = sum + i
-print(sum)
-
 #div_by: int int -> bool
 def div_by(x, multiple):
     if (x % multiple == 0):
         return true
     else:
         return false
+
+for i in range(0, 1000):
+    if (div_by(i, 3)):
+        sum = sum + i
+    elif (div_by(i, 5)):
+        sum = sum + i
+print(sum)
