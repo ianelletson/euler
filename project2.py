c88e46b8acfc648734a27133d725e53ec0b3f293
ianelletson <ianelletson@gmail.com>
2013-06-18 13:25:10 -0500

    Reimplemented while loop, added break

--- project2.py
+++ project2.py
@@ -5,8 +5,10 @@ def fib(limit):
     num1 = 0
     num2 = 1
     num3 = 0
-    for i in range(0,10):
+    while True:
         num3 = num1 + num2
         num1 = num2
         num2 = num3
         print(num2)
+        if (num3 >= limit):
+            break
