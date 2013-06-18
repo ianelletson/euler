f33788d148f5c7d786b59ea3a78e93843a933688
ianelletson <ianelletson@gmail.com>
2013-06-18 13:21:03 -0500

    Working sequence generation

--- project2.py
+++ project2.py
@@ -2,10 +2,10 @@
 
 #fib: int -> void
 def fib(limit):
-    num1 = 1
-    num2 = 2
+    num1 = 0
+    num2 = 1
     num3 = 0
-    for (i in range(1,10)):
+    for i in range(0,10):
         num3 = num1 + num2
         num1 = num2
         num2 = num3
