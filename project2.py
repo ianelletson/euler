20123f28e48ddfc46cf54c393da196a4c1edfb8b
ianelletson <ianelletson@gmail.com>
2013-06-18 11:19:21 -0500

    Changed while loop params

--- project2.py
+++ project2.py
@@ -13,5 +13,6 @@ def fib(exceed):
         fibCount2 = fibCount3
         if (fibCount3 % 2 == 0):
             fibSum = fibSum + fibCount3
+        exceed = fibCount3
         i = i + 1
     print(fibSum)
