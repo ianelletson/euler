51cd5537081414a6bf0b806ec3b1b1146c7d4ca9
ianelletson <ianelletson@gmail.com>
2013-06-18 09:11:48 -0500

    Project Euler project 1 simple for loop with method

---
+++ project1.py
@@ -0,0 +1,17 @@
+#Project 1
+
+sum = 0
+
+for i in range(0, 1000):
+    if (div_by(i, 3)):
+        sum = sum + i
+    else if (div_by(i, 5)):
+        sum = sum + i
+print(sum)
+
+#div_by: int int -> bool
+def div_by(x, multiple):
+    if (x % multiple == 0):
+        return true
+    else:
+        return false
