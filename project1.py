e7f8f8254fb1dd30e8a42a89f78b63ee6062e2e1
ianelletson <ianelletson@gmail.com>
2013-06-18 09:17:44 -0500

    more robust, made funciton of for loop

--- project1.py
+++ project1.py
@@ -1,7 +1,5 @@
 #Project 1
 
-sum = 0
-
 #div_by: int int -> bool
 def div_by(x, multiple):
     if (x % multiple == 0):
@@ -9,9 +7,12 @@ def div_by(x, multiple):
     else:
         return false
 
-for i in range(0, 1000):
-    if (div_by(i, 3)):
-        sum = sum + i
-    elif (div_by(i, 5)):
-        sum = sum + i
-print(sum)
+#printSum: int int int -> void
+def printSum(range, div1, div2):
+    sum = 0
+    for i in range(0, range):
+        if (div_by(i, div1)):
+            sum = sum + i
+        elif (div_by(i, div2)):
+            sum = sum + i
+    print(sum)
